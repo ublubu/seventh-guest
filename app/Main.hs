ef40eec82074c914270ac7c95b0763243343284b
Kynan Rilee <kynan.rilee@gmail.com>
2018-03-13 15:28:29 -0700

    choosing moves

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,12 @@
 
 module Main where
 
-import ClassyPrelude
+import           ClassyPrelude
 
-import Control.Monad.ST
-import qualified Data.Vector.Mutable as MV
+import           Control.Monad.ST
+import qualified Data.Text           as T
 import qualified Data.Vector.Generic as V
-import qualified Data.Text as T
+import qualified Data.Vector.Mutable as MV
 
 {- |
 Grid UI:
@@ -34,8 +34,8 @@ type BoardState s = Grid s Square
 type BoardUI s = Grid s Char
 
 showSquare :: Square -> Char
-showSquare Blue = 'X'
-showSquare Green = 'O'
+showSquare Blue  = '+'
+showSquare Green = '='
 showSquare Empty = '_'
 
 dim :: Int
@@ -59,8 +59,90 @@ blankGrid tile =
   MV.replicateM dim row
   where row = MV.replicate 7 tile
 
+setTile :: (Int, Int) -> tile -> Grid s tile -> ST s ()
+setTile (irow, icol) tile rows = do
+  row <- MV.unsafeRead rows irow
+  MV.unsafeWrite row icol tile
+
+getTile :: (Int, Int) -> Grid s tile -> ST s tile
+getTile (irow, icol) rows = do
+  row <- MV.unsafeRead rows irow
+  MV.unsafeRead row icol
+
+findTiles :: (tile -> Bool) -> Grid s tile -> ST s [(Int, Int)]
+findTiles predicate rows = foldlM f [] indices
+  where
+    f accum irow = do
+      row <- MV.unsafeRead rows irow
+      let f' accum' icol = do
+            tile <- MV.unsafeRead row icol
+            if predicate tile
+              then return $ (irow, icol) : accum'
+              else return accum'
+      (<> accum) <$> foldlM f' [] indices
+
+data Move
+  = Jump (Int, Int)
+         (Int, Int)
+  | Spawn (Int, Int)
+  deriving (Show, Eq)
+
+validMoves :: (Int, Int) -> BoardState s -> ST s [Move]
+validMoves source@(irow, icol) rows = do
+  spawns <- fmap Spawn <$> filterM emptyTile spawns_
+  jumps <- fmap (Jump source) <$> filterM emptyTile jumps_
+  return $ jumps <> spawns
+  where
+    emptyTile (irow, icol) =
+      if irow < 0 || irow >= dim || icol < 0 || icol >= dim
+        then return False
+        else do
+          tile <- getTile (irow, icol) rows
+          return $ tile == Empty
+    spawns_ =
+      [ (irow - 1, icol - 1)
+      , (irow - 1, icol)
+      , (irow - 1, icol + 1)
+      , (irow, icol - 1)
+      , (irow, icol + 1)
+      , (irow + 1, icol - 1)
+      , (irow + 1, icol)
+      , (irow + 1, icol + 1)
+      ]
+    jumps_ =
+      [ (irow - 2, icol - 2)
+      , (irow - 2, icol - 1)
+      , (irow - 2, icol)
+      , (irow - 2, icol + 1)
+      , (irow - 2, icol + 2)
+      , (irow - 1, icol - 2)
+      , (irow - 1, icol + 2)
+      , (irow, icol - 2)
+      , (irow, icol + 2)
+      , (irow + 1, icol - 2)
+      , (irow + 1, icol + 2)
+      , (irow + 2, icol - 2)
+      , (irow + 2, icol - 1)
+      , (irow + 2, icol)
+      , (irow + 2, icol + 1)
+      , (irow + 2, icol + 2)
+      ]
+
 main :: IO ()
 main = do
   putStrLn "Hi!"
-  grid <- stToIO $ blankGrid Empty
-  putStrLn =<< stToIO (showGrid showSquare grid)
+  board <- stToIO $ blankGrid Empty
+  printBoard board
+  stToIO $ do
+    setTile (0, 0) Blue board
+    setTile (6, 6) Blue board
+    setTile (0, 6) Green board
+    setTile (6, 0) Green board
+  printBoard board
+  blues <- stToIO $ findTiles (== Blue) board
+  print blues
+  moves <- stToIO $ validMoves (0, 0) board
+  print moves
+  where printBoard board = do
+          putStrLn =<< stToIO (showGrid showSquare board)
+          putStrLn ""
